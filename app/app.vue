f0746d059a9910eea5cd1be1c12073864874aa6f
oluwadamilolaaderogba@gmail.com <oluwadamilolaaderogba@gmail.com>
2026-07-20 19:46:12 +0100

    feat: added global script

--- app/app.vue
+++ app/app.vue
@@ -4,3 +4,30 @@
     <NuxtPage/>
   </div>
 </template>
+
+<script lang="ts">
+import { fetchCurrentUser } from './composables/useAuth'
+import { useLocalStorage } from './composables/useLocalStorage'
+
+interface User {
+    id: number,
+    name?: string
+}
+
+interface FetchCurrentUserResponse {
+    user?: User
+}
+
+const { value: user } = useLocalStorage<User | null>(
+    "user",
+    null
+)
+
+onMounted(async () => {
+    const me = await fetchCurrentUser() as FetchCurrentUserResponse
+
+    if (me.user) {
+        user.value = me.user
+    }
+})
+</script>
