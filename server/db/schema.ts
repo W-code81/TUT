c1c1d479abc65d10b5b90ffb3f19a48fabc7fcb9
oluwadamilolaaderogba@gmail.com <oluwadamilolaaderogba@gmail.com>
2026-07-22 08:46:32 +0100

    feat: updated schema and renamed column

--- server/db/schema.ts
+++ server/db/schema.ts
@@ -1,16 +1,19 @@
 import { sqliteTable, int, text } from "drizzle-orm/sqlite-core";
 
 const fruitsTable = sqliteTable("fruits", {
-    id: int().primaryKey({autoIncrement: true}),
+    id: int().primaryKey({ autoIncrement: true }),
     name: text().notNull(),
 })
 
 const userTable = sqliteTable("users", {
-    id: int().primaryKey({autoIncrement: true}),
-    name: text().notNull(),
+    id: int().primaryKey({ autoIncrement: true }),
+    email: text().notNull(),
+    nickname: text().notNull(),
     password: text().notNull(),
     resetPasswordToken: text(), // allow null when no reset token is set (text() is nullable by default)
     resetPasswordExpires: int(),// integer builder in this setup is nullable by default, so omit .nullable()
+    createdAt: int(),
+    updatedAt: int(),
 })
 
 export default fruitsTable;
